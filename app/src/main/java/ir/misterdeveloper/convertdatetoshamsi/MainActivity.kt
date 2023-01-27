4d90e93f89401fd4796bdf942f37ce70a76352fe
daris <mohaammaadsdeq@gmail.com>
2023-01-28 03:25:06 +0330

    first commit

--- app/src/main/java/ir/misterdeveloper/convertdatetoshamsi/MainActivity.kt
+++ app/src/main/java/ir/misterdeveloper/convertdatetoshamsi/MainActivity.kt
@@ -2,10 +2,7 @@ package ir.misterdeveloper.convertdatetoshamsi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ir.misterdeveloper.datetoshamsi.PersianDate
-import ir.misterdeveloper.datetoshamsi.PersianTime
-import ir.misterdeveloper.datetoshamsi.PersianTimeAndDate
-import ir.misterdeveloper.toastcustom.ToastCustom
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
